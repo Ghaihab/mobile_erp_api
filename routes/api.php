438c80b8540f4f8ba0c544a00212a7e0d97b76c4
Ghaihab Almuhanna <g.almuhanna@tamkeentech.sa>
2019-03-30 22:21:10 +0300

    lloookpup

--- routes/api.php
+++ routes/api.php
@@ -46,6 +46,6 @@ Route::post('custody/request/reject/{id}', CustodyRequestsController::class . "@
 
 //------------------------------------------------ Employee Custodies ------------------------------------------------//
 Route::get('custodies', EmployeeController::class . "@custodies");
-Route::get('lookup/cutodies', function(){
+Route::get('lookup/custodies', function(){
     return \App\Models\Custody::all();
 });
